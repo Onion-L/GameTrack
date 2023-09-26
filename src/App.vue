f81df46d6d548818a7e3d3fe13dd424e86e555fd
Xiang Li <lixiang020317@gmail.com>
2023-09-26 16:10:23 +0100

    login page

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <LoginPage></LoginPage>
+  </div>
+</template>
+<script setup>
+  import LoginPage from './view/Login/LoginPage.vue';
+</script>
+<style lang="">
+  
+</style>
