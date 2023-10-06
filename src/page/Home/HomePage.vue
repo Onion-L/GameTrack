de2cfdc88127ab07071cf4c2609e177c145766d6
Xiang Li <lixiang020317@gmail.com>
2023-10-06 23:12:05 +0100

    update:intro page

--- src/page/Home/HomePage.vue
+++ src/page/Home/HomePage.vue
@@ -1,58 +1,56 @@
 <script setup>
 
+import {Menu as ElMenu} from "@element-plus/icons-vue";
 </script>
 
 <template>
   <div class="home-container">
-<!--    <div class="overlay"></div>-->
+    <div class="overlay"></div>
     <div class="nav-container">
       <div class="logo-section">
         <img class="gt-logo-img" src="../../assets/logo.png" alt="logo">
-        <span>GameTrack</span>
       </div>
-      <div class="flex-grow"></div>
-      <div class="menu-icon">
-        <el-icon><Menu /></el-icon>
+<!--      <div class="flex-grow"></div>-->
+      <div class="menu-icon" style="color:#fff; width: 50px;">
+        <el-icon size="30"><ElMenu /></el-icon>
       </div>
     </div>
-    <div class="index-1">
-<!--      <div style="position:absolute;font-size: 100px; text-align: center; color:#fff">
-        GameTrack
-      </div>
-      <div style="width: 400px">
+    <div class="title" style="position: absolute; margin-left: 60px; margin-top: 50px">
+      <span style="font-size:160px;color:#fff;font-family: 'Caveat',sans-serif; ">GameTrack</span>
+      <div style="width: 550px; font-size: 24px; color: #fff">
         Welcome to the future of sports performance analysis! Our Player Data Analysis Platform is your
         key to unlocking a world of insights, improving your performance, and dominating your competition.
-      </div>-->
+      </div>
     </div>
     <div class="index-1">
-      <div style=" position:absolute;font-size: 50px; margin-top: 100px; margin-left: 30px; color:#fff">
-        Unlock Your Full Potential with Player Data Insights
-      </div>
-      <div style="width: 400px">
-        Welcome to the future of sports performance analysis! Our Player Data Analysis Platform is your key to unlocking a world of insights, improving your performance, and dominating your competition.
-      </div>
+    </div>
+    <div class="index-2">
+
     </div>
   </div>
 </template>
 
 <style scoped lang="less">
+@import "../../style/variable.less";
+@font-face {
+  font-family: 'Caveat';
+  src: url('../../assets/fonts/Caveat-Bold.ttf');
+}
+
 .home-container {
   position: relative;
   overflow: hidden;
-  background:url("../../assets/home-bg-image.jpg") no-repeat center fixed;
+  background:url("../../assets/home-bg-image.png") no-repeat center fixed;
+  //filter: blur(5px);
   background-size: cover;
   .nav-container {
-    //background-color: rgba(0, 0, 0, 0.8);
+    //background-color: rgba(0, 0, 0, 0.6);
+    width: 100vw;
+    position: fixed;
     display: flex;
+    padding: 5px 20px;
     .logo-section {
       flex: 1;
-    }
-    .flex-grow {
-      flex: 2;
-    }
-    .menu-icon {
-
-      flex: 0.5;
     }
     .gt-logo-img {
       width: 50px;
@@ -65,13 +63,18 @@
     top: 0;
     left: 0;
     width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5); /* 半透明黑色蒙层 */
+    height: 100vh;
+    background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.9)); /* 半透明黑色蒙层 */
   }
   .index-1 {
     width: 100vw;
     height: 100vh;
-    padding: 10px 20px;
+  }
+  .index-2 {
+    width:100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.9);
+    margin: 0 auto;
   }
 }
 
